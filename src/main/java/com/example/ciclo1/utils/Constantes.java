00b18e304af09cf7a770daa9ef8d47682814f5bc
RicharLeon <rleon97@hotmail.com>
2024-01-23 15:33:22 -0500

    Se crea la primera rama RICHAR.

--- src/main/java/com/example/ciclo1/utils/Constantes.java
+++ src/main/java/com/example/ciclo1/utils/Constantes.java
@@ -3,6 +3,8 @@ package com.example.ciclo1.utils;
 public class Constantes {
 
     public static final String CARDIOLOGIA = "Atención a pacientes con problemas cardíacos";
+
+    public static final String example = "Mi rama richar";
     public static final String DIAGNOSTICOFINAL = "Estable";
 
     private Constantes(){
