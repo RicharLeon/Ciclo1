b86b4857ef2aaa0c20694b93fa56a439343346f3
Juan-Restrepo-B <juanprestrepob@gmail.com>
2024-01-28 21:16:13 -0500

    Coreccion

--- src/main/java/com/example/ciclo1/controllers/HabitacionesControllers.java
+++ src/main/java/com/example/ciclo1/controllers/HabitacionesControllers.java
@@ -4,24 +4,26 @@ import com.example.proyecto1.models.dao.IHabitacionesService;
 import com.example.proyecto1.models.dao.HabitacionesConsultaDTO;
 import com.example.proyecto1.models.dao.Habitaciones;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/habitaciones")
 
 public class HabitacionesControllers {
+    Map<String, Object> response = new HashMap<>();
 
     @Autowired
     private IHabitacionesService habitacionesService;
 
-    @GetMapping("/consulta")
-    public List<Habitaciones> getAllHabitaciones() {
-        return habitacionesService.findAll();
+    @GetMapping("/{id}")
+    public ResponseEntity<?>mostrarHabitacionesPorIdDepartamento(@PathVariable Long id) {
+        return new ResponseEntity<>(habitacionesService.mostrarHabitacionesPorIdDepartamento(id), HttpStatus.OK);
     }
 }
--- src/main/java/com/example/ciclo1/models/services/IHabitacionesService.java
+++ src/main/java/com/example/ciclo1/models/services/IHabitacionesService.java
@@ -3,5 +3,5 @@ package com.example.proyecto1.models.dao;
 import java.util.List;
 import com.example.proyecto1.models.dao.Habitaciones;
 public interface IHabitacionesService {
-    List<Habitaciones> findAll();
+    List<Habitaciones> mostrarHabitacionesPorIdDepartamento(Long id);
 }
